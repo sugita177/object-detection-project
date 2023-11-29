70e3ede8770f04faad985de3f645de31f4ee2d6d
sugita177 <moriaki.sugita.1730@gmail.com>
2023-11-29 11:34:19 +0900

    データベースのセットアップ完了

--- apps/app.py
+++ apps/app.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 
@@ -9,9 +10,18 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_mapping(
+    csrf = CSRFProtect()
 
+    app.config.from_mapping(
+        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+        SQLALCHEMY_DATABASE_URI=(
+            f"sqlite:///{Path(__file__).parent.parent /'local.sqlite'}"),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
     )
+
+    csrf.init_app(app)
 
     db.init_app(app)
 
